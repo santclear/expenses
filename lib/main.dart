6480b638276f54cfac71156966e2629c194dcc62
Sant'Clear <santclear@gmail.com>
2022-11-29 14:36:43 -0300

    05 - Created new Transaction class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,53 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-
   const ExpensesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage()
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Running Shoes',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+        id: 't2',
+        title: 'Electricity bill',
+        value: 211.30,
+        date: DateTime.now()),
+  ];
+
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Expenses'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const <Widget>[
-          SizedBox(
-            child: Card(
-              color: Colors.blue,
-              elevation: 5,
-              child: Text('Chart'),
+        appBar: AppBar(
+          title: const Text('Expenses'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const <Widget>[
+            SizedBox(
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Chart'),
+              ),
             ),
-          ),
-          Card(
-            child: Text('Transaction List')
-          )
-        ],
-      )
-    );
+            Card(child: Text('Transaction List'))
+          ],
+        ));
   }
 }
