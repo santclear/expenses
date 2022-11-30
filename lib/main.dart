ed16c66a00599bb6cdca4adaf0ba42c3a95a7abe
Sant'Clear <santclear@gmail.com>
2022-11-29 22:38:37 -0300

    09 - Stylized transaction value box.

--- lib/main.dart
+++ lib/main.dart
@@ -58,12 +58,17 @@ class MyHomePage extends StatelessWidget {
                         ),
                         decoration: BoxDecoration(
                             border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         )),
                         padding: const EdgeInsets.all(10),
                         child: Text(
                           tr.value.toString(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
                         ),
                       ),
                       Column(
