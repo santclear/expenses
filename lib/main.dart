78fc79741a053eb73624160516c512c137d0a8e2
Sant'Clear <santclear@gmail.com>
2022-11-29 22:37:20 -0300

    08 - Stylized transaction components.

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,16 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                     children: <Widget>[
                       Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        )),
+                        padding: const EdgeInsets.all(10),
                         child: Text(
                           tr.value.toString(),
                         ),
