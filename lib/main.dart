5de5efde6ce83bc9c6456fcde846c22e55edbe13
Sant'Clear <santclear@gmail.com>
2022-12-01 21:40:58 -0300

    12 - Added Intl dependency and Formatted transaction date.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -82,7 +83,7 @@ class MyHomePage extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('MMM d y').format(tr.date),
                             style: const TextStyle(
                               color: Colors.grey,
                             ),
