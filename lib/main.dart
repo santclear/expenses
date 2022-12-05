3c7b3bf7be8982e06397aabf1988c1f3a6f4e67b
Sant'Clear <santclear@gmail.com>
2022-12-04 22:04:14 -0300

    24 - Closed the TransactionForm modal after clicking submit.

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {
