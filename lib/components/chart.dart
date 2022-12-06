84609ba60ed5e1a3c8e6e3e2e57e852e1ee9aa6e
Sant'Clear <santclear@gmail.com>
2022-12-06 18:16:18 -0300

    31 - Implementing the chart component.

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -25,12 +25,9 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E().format(weekDay)[0]);
-      print(totalSum);
-
       return {
         'day': DateFormat.E().format(weekDay)[0],
-        'value': 9.99,
+        'value': totalSum,
       };
     });
   }
@@ -42,7 +39,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: const EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactions.map((tr) {
+          return Text('${tr['day']}: ${tr['value']}');
+        }).toList(),
       ),
     );
   }
